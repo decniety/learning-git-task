b003a54a2338624ee28ff3b9bff28bcd21f5326e
Michal Dec <decniety@gmail.com>
2023-09-27 19:45:38 -0400

    Lista

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 list = {"piekarnia":["chleb", "pączek", "bułki"], 
         "warzywniak":["marchew", "seler", "rukola"]}
-
-new_list = dict((k.capitalize(), v) for k, v in list.items())
-new_list2 = {key: [val.capitalize() for val in value] for key, value in new_list.items()}
-
-for key, value in new_list2.items():
-
-    print (f"Idę do {key}, kupuje tu następujące rzeczy: {value}")
-length = len(list["piekarnia"])+len(list["warzywniak"])
-
-print(f"W sumie kupuje: {length} produktów")
